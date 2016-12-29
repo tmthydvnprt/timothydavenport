33869bb2fe43b7a345999d78666dd11c838dd90d
tmthydvnprt <tim@tmthydvnprt.com>
2016-12-29 12:39:59 -0800

    publish from last master commit: update font size

--- js/tim.js
+++ js/tim.js
@@ -1,6 +1,22 @@
-/*!
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * asset tim.js combined on 2016-12-29T12:28:24-08:00
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
-$(document).ready(function() { $.getJSON("json/tim.json", function(data) { var all=$('*'), headspin=$('#headspin'), i=0, L=data.length, pause=false; window.setInterval(function() { if (!pause) { headspin.text(data[i]); i=(i + 1) % L; } }, 20); all.on('click', function() { pause=!pause; }); }); } );
+$(document).ready(function() {
+    // Load ascii "video"
+    $.getJSON("json/tim.json", function(data) {
+        // Set up
+        var all = $('*'),
+            headspin = $('#headspin'),
+            i = 0,
+            L = data.length,
+            pause = false;
+        // Setup interval to draw each frame
+        window.setInterval(function() {
+            if (!pause) {
+                headspin.text(data[i]);
+                i = (i + 1) % L;
+            }
+        }, 20);
+        // Toggle pause
+        all.on('click', function() {
+            pause = !pause;
+        });
+    });
+} );
