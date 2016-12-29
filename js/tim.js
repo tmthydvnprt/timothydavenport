4d530a99b001fab4ccfcf2ed7d1f42d102005b60
tmthydvnprt <tim@tmthydvnprt.com>
2016-12-29 12:13:49 -0800

    publish from last master commit: remove FOUT

--- js/tim.js
+++ js/tim.js
@@ -1,6 +1,11 @@
-/*!
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * asset tim.js combined on 2016-12-29T08:19:46-08:00
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
-$(document).ready(function() { $.getJSON("json/tim.json", function(data) { var headspin=$('#headspin'), i=0, L=data.length; window.setInterval(function() { headspin.text(data[i]); i=(i + 1) % L; }, 20); }); } );
+$(document).ready(function() {
+    $.getJSON("json/tim.json", function(data) {
+        var headspin = $('#headspin'),
+            i = 0,
+            L = data.length;
+        window.setInterval(function() {
+            headspin.text(data[i]);
+            i = (i + 1) % L;
+        }, 20);
+    });
+} );
