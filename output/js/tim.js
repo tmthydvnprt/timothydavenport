e2df127eb541bf5e436759be27086662bffbc331
tmthydvnprt <tim@tmthydvnprt.com>
2016-12-30 09:10:47 -0800

    improve font handling and 404 page

--- output/js/tim.js
+++ output/js/tim.js
@@ -1,19 +1,47 @@
 $(document).ready(function() {
-    // Load ascii "video"
-    $.getJSON("json/tim.json", function(data) {
-        // Set up
-        var all = $('*'),
-            headspin = $('#headspin'),
+    // Set up elements
+    var all = $('*'),
+        headspin = $('#headspin'),
+        fourOhfour = $('#fourOhfour'),
+        ascii_vid_path = '',
+        elem = null;
+
+    // Decide which to use
+    if (headspin.length) {
+        ascii_vid_path = "json/tim.json";
+        ascii_elem = headspin;
+    } else {
+        ascii_vid_path = "json/404.json";
+        ascii_elem = fourOhfour;
+    }
+
+    // Load ASCII "video"
+    $.getJSON(ascii_vid_path, function(data) {
+        var pause = false,
             i = 0,
-            L = data.length,
-            pause = false;
+            h = 0,
+            L = data.length;
+
         // Setup interval to draw each frame
         window.setInterval(function() {
             if (!pause) {
-                headspin.text(data[i]);
-                i = (i + 1) % L;
+                if (headspin.length) {
+                    ascii_elem.text(data[i]);
+                    i = (i + 1) % L;
+                } else {
+                    h = (i + Math.floor(L / 2)) % L;
+                    ascii_elem.text(
+                          data[i] + '\n'
+                        + data[h] + '\n'
+                        + data[i] + '\n'
+                        + data[h] + '\n'
+                        + data[i]
+                    );
+                    i = (i + 1) % L;
+                }
             }
         }, 20);
+
         // Toggle pause
         all.on('click', function() {
             pause = !pause;
